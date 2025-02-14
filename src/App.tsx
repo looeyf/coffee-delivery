41ba4ddbace60c32d646a537576f848011a416c5
Luis Oliveira <luis10.pf@gmail.com>
2025-02-13 23:28:04 -0300

    feat: reset css and setup theme/colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+import { ResetCssGlobalStyle } from "./styles/global";
+
 function App() {
-  return <h1>Coffee Delivery</h1>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <ResetCssGlobalStyle />
+
+      <h1>Coffee Delivery</h1>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
